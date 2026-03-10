0ea8bd431ab49cf90c86b53a4438c58e09a925a6
ofurkancoban <ofurkancoban@gmail.com>
2026-03-10 14:07:05 +0100

    style: use exactly the user provided new-chat-outline svg path

--- content.js
+++ content.js
@@ -936,8 +936,8 @@ function injectFixedNewChatButton() {
         btn = document.createElement('div');
         btn.id = 'waw-fixed-new-chat';
         btn.setAttribute('title', 'Yeni Sohbet');
-        // WhatsApp standard 'chat' SVG (Orijinal kareli ikon)
-        btn.innerHTML = `<svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 24 24"><title>chat</title><path fill="currentColor" d="M19.005,3.175H4.674C3.642,3.175,3,3.789,3,4.821V21.02l3.544-3.514h12.461c1.033,0,2.064-1.06,2.064-2.093V4.821C21.069,3.789,20.038,3.175,19.005,3.175z M14.016,13.044H7.041V11.1h6.975V13.044z M17.016,9.044H7.041V7.1h9.975V9.044z"></path></svg>`;
+        // Orijinal (Kullanıcının talep ettiği) 'new-chat-outline' SVG
+        btn.innerHTML = `<span aria-hidden="true" data-icon="new-chat-outline" class="xxk0z11 xvy4d1p"><svg viewBox="0 0 24 24" height="24" width="24" preserveAspectRatio="xMidYMid meet" class="" fill="none"><title>new-chat-outline</title><path d="M9.53277 12.9911H11.5086V14.9671C11.5086 15.3999 11.7634 15.8175 12.1762 15.9488C12.8608 16.1661 13.4909 15.6613 13.4909 15.009V12.9911H15.4672C15.9005 12.9911 16.3181 12.7358 16.449 12.3226C16.6659 11.6381 16.1606 11.0089 15.5086 11.0089H13.4909V9.03332C13.4909 8.60007 13.2361 8.18252 12.8233 8.05119C12.1391 7.83391 11.5086 8.33872 11.5086 8.991V11.0089H9.49088C8.83941 11.0089 8.33411 11.6381 8.55097 12.3226C8.68144 12.7358 9.09947 12.9911 9.53277 12.9911Z" fill="currentColor"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M0.944298 5.52617L2.99998 8.84848V17.3333C2.99998 18.8061 4.19389 20 5.66665 20H19.3333C20.8061 20 22 18.8061 22 17.3333V6.66667C22 5.19391 20.8061 4 19.3333 4H1.79468C1.01126 4 0.532088 4.85997 0.944298 5.52617ZM4.99998 8.27977V17.3333C4.99998 17.7015 5.29845 18 5.66665 18H19.3333C19.7015 18 20 17.7015 20 17.3333V6.66667C20 6.29848 19.7015 6 19.3333 6H3.58937L4.99998 8.27977Z" fill="currentColor"></path></svg></span>`;
         
         btn.addEventListener('click', () => {
              // Orijinal "Yeni Sohbet" butonlarını ara (birden fazla olası yer)
